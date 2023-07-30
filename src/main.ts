c99bfe9a1cf3c3e210a742919b599ed62d19afda
Ewa Olczak <ewa.olczak@gmail.com>
2023-07-30 18:08:28 +0200

    Add interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { PrismaService } from './shared/services/prisma.service';
+import { LoggerInterceptor } from './shared/interceptors/logger.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalInterceptors(new LoggerInterceptor());
   app.setGlobalPrefix('api');
 
   const prismaService = app.get(PrismaService);
