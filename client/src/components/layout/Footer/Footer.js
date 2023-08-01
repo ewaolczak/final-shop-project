138de8006542953bf91b261f18d92faf0df03466
Ewa Olczak <ewa.olczak@gmail.com>
2023-08-01 20:20:55 +0200

    Add logo and footer and navbar styles

--- client/src/components/layout/Footer/Footer.js
+++ client/src/components/layout/Footer/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="py-3 text-muted text-center">
-      Copyright &copy; myShopApp 2023
+      Copyright &copy; coffee shop 2023
     </div>
   );
 };
--- client/src/components/layout/NavBar/NavBar.js
+++ client/src/components/layout/NavBar/NavBar.js
@@ -2,19 +2,26 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartShopping, faHouse } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCartShopping,
+  faHouse,
+  faMugHot,
+} from '@fortawesome/free-solid-svg-icons';
 
 const NavBar = () => {
   return (
-    <Navbar bg="primary" variant="dark" className="mt-4 mb-4 rounded">
+    <Navbar bg="warning" variant="light" className="mt-4 mb-4 rounded">
       <div className="container">
-        <Navbar.Brand href="/">myShopApp</Navbar.Brand>
-        <Nav className="me-auto row justify-content-end">
+        <Navbar.Brand href="/">
+          <FontAwesomeIcon icon={faMugHot} size="xl" className="me-2" />
+          coffee shop
+        </Navbar.Brand>
+        <Nav className="me-0 d-flex flex-row">
           <Nav.Link as={NavLink} to="/">
-            <FontAwesomeIcon icon={faHouse} size="lg" className="mr-1" />
+            <FontAwesomeIcon icon={faHouse} size="lg" />
           </Nav.Link>
           <Nav.Link as={NavLink} to="/cart">
-            <FontAwesomeIcon icon={faCartShopping} size="lg" className="mr-2" />
+            <FontAwesomeIcon icon={faCartShopping} size="lg" />
           </Nav.Link>
         </Nav>
       </div>
